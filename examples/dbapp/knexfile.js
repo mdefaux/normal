473daf6a245e466e7b37154dc7e426cf2d5d3b0b
Maurice Defaux <defauxmaurice@yahoo.com>
2022-06-28 17:09:15 +0200

    db app base seeding file

--- examples/dbapp/knexfile.js
+++ examples/dbapp/knexfile.js
@@ -28,36 +28,4 @@ module.exports = {
       }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
