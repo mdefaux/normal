a424faab027323e32b3d25085a0a542a77b5409f
Maurice Defaux <defauxmaurice@yahoo.com>
2022-06-29 11:47:08 +0200

    Test model and url parse

---
+++ examples/dbapp/test/urlparse.test.js
@@ -0,0 +1,44 @@
+describe('URL parse test', function() {
+
+    it('should return a id_customer = 4 filter on entity asset', function(done) {
+        chai.request(server)
+            .get('/orm/asset/all?iCustomer[]=4')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json; // si assicura che il risultato sia json
+                res.body.should.be.a('array');
+                res.body.length.should.equal(1); // number of entity
+                res.body[0].should.have.property('column');
+                res.body[0].column.should.equal('id_customer');
+                done();
+            });
+    });
+    
+    it('should return a id_customer group by on entity asset', function(done) {
+        chai.request(server)
+            .get('/orm/asset/all?gbCustomer[]')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json; // si assicura che il risultato sia json
+                res.body.should.be.a('array');
+                res.body.length.should.equal(1); // number of entity
+                res.body[0].should.have.property('column');
+                res.body[0].column.should.equal('id_customer');
+                done();
+            });
+    });
+    
+    it('should return a "part number" field on entity asset', function(done) {
+        chai.request(server)
+            .get('/orm/asset/all?slpart_number[]')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json; // si assicura che il risultato sia json
+                res.body.should.be.a('array');
+                res.body.length.should.equal(1); // number of entity
+                res.body[0].should.have.property('column');
+                res.body[0].column.should.equal('part_number');
+                done();
+            });
+    });
+});
