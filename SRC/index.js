7834cc709eec17de7c5a8b5c5479b3cbd20ac900
Maurice Defaux <defauxmaurice@yahoo.com>
2022-10-03 16:14:01 +0200

    Class Query exported by library index

--- SRC/index.js
+++ SRC/index.js
@@ -3,11 +3,14 @@ const StoreHost = require( './orm/StoreHost' )
 const {URLquery} = require( './url_query' );
 
 const {KdbStoreHost} = require( './orm/kdbhost/KdbHost' );
+const { Query } = require('./orm/Query');
 
 module.exports= {
     URLquery: URLquery,
     store: defs,
     StoreHost: StoreHost,
+    Query: Query,
+    
 
     // KDB
     KdbStoreHost: KdbStoreHost
