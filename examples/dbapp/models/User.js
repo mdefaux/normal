b8ddb5c04072ba8cc62120b4b09ef243621634b4
Maurice Defaux <defauxmaurice@yahoo.com>
2022-11-17 18:54:33 +0100

    readme example update

--- examples/dbapp/models/User.js
+++ examples/dbapp/models/User.js
@@ -2,24 +2,23 @@
  * Creates a simple model of User based on a db table 
  * called user_t.
  */
- const store = require("./store");
+const store = require("./store");
 
- /**User model is a simple table with 
-  * domain name and name
-  * 
-  */
- var User = store.entity( "User", (model) => {
- 
-     // there is a table called 'user_t' on DB
-     model.source( "user_t" );
- 
-     // defines the columns/fields of thi model
-     model.id( "id" );
-     model.label('domain_name');   // there is a string field domain_name that id used as label for that row
+/**User model is a simple table with 
+ * domain name and name
+ * 
+ */
+var User = store.entity("User", (model) => {
 
-    // model.label('domainName')
-    //     .source( "domain_name" );
-     model.string("name");          // name of user
- } );
- 
- module.exports = User;
+    // there is a table called 'user_t' on DB
+    model.source("user_t");
+
+    // defines the columns/fields of this model
+    model.id("id");
+    model.label('domainName')    // domainName is used as 'label' for this entity
+        .source('domain_name');     // domain_name is the name of the column on the table
+
+    model.string("name");          // name of user
+});
+
+module.exports = User;
