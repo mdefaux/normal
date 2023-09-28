41e8a96eb91127df3152dda299529e63e38d845b
Maurice Defaux <defauxmaurice@yahoo.com>
2023-09-28 22:23:16 +0200

    less function test

--- examples/dbapp/test/less-functions.test.js
+++ examples/dbapp/test/less-functions.test.js
@@ -5,10 +5,22 @@ const assert = require( "assert" );
 describe( "less functions", function () {
     it( "returns the less value of ", async function () {
         
-        let rs = await Device.select( Device.partnumber_id.lessThan(4) )
+        let rs = await Device.select('*') 
+            .where( Device.serialnumber.lessThan('SN004') )
             .exec();
             
-        assert( rs.length < 2 );
+        assert( rs.length < 4 );
+      //  assert( rs[0].max );
+      //  assert( typeof rs[0].max === 'string'  );
+     //   assert( rs[0].max === 'SN005' );
+    });
+    it( "returns the less value of object link", async function () {
+        
+        let rs = await Device.select('*') 
+            .where( Device.Partnumber.lessThan(4) ) // uses id
+            .exec();
+            
+        assert( rs.length < 4 );
       //  assert( rs[0].max );
       //  assert( typeof rs[0].max === 'string'  );
      //   assert( rs[0].max === 'SN005' );
