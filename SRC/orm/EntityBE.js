26a6082535140408d9ad55c8c67580624b8e73fc
Maurice Defaux <defauxmaurice@yahoo.com>
2023-10-24 18:10:43 +0200

    Entity BE compare method prototype

--- SRC/orm/EntityBE.js
+++ SRC/orm/EntityBE.js
@@ -307,6 +307,83 @@ class EntityBE {
         return this.actionDictionary[ actionName ];
     }
 
+    async compare( sourceQuery, destQuery, parameters, chunk = 0 ) {
+        let notInSource = {};
+        let notInDest = {};
+        let match = {};
+
+        let sourcePageSize = parameters.sourcePageSize || 500;
+        let keyFieldSource = parameters.keyFieldS || "id";
+        let keyFieldDest = parameters.keyFieldD || "id";
+        // let insertSize = parameters.insertSize || 500;
+        // let keyFieldS = parameters.keyFieldS || "id";
+        // let keyFieldD = parameters.keyFieldD || "id";
+        sourceQuery.page( chunk+1, sourcePageSize );
+        let sourceRsChunk = await sourceQuery.exec();
+        // console.log(arrayA);
+
+        let keyToFind = sourceRsChunk.map( (rec) => rec[keyFieldSource] );
+
+        // 
+        // let pageSize = parameters.destinationPageSize || 500;
+        destQuery.page(null, sourcePageSize*2);
+
+        // finds all destination records which have the key field
+        let destRs = await destQuery.where( this[keyFieldDest].in( keyToFind ) ).exec();
+
+        if ( destRs.length > sourceRsChunk.length ) {
+            console.warn( `Destination records (${destRs.length}) greater than source records (${sourceRsChunk.length})` );
+            // destRs.sort( (a,b)=>a[keyFieldDest]<b[keyFieldDest] )
+        }
+
+        let result = destRs.reduce( ( acc, rec ) => {
+
+            if ( acc.matches[ rec[keyFieldDest] ] ) {
+                return { 
+                    matches: acc.matches, 
+                    duplicateKeys: [...acc.duplicateKeys, rec[keyFieldDest] ] 
+                }
+            }
+            return { 
+                matches: {...acc.matches, [rec[keyFieldDest]]: rec }, 
+                duplicateKeys: acc.duplicateKeys 
+            }
+        }, { matches: {}, duplicateKeys: [] } )
+
+        // builds a map with the result
+
+        match = result.matches;
+        
+        if ( result.duplicateKeys.length > 0 ) {
+            console.warn( `Destination keys duplicates: (${result.duplicateKeys.length})` );
+            console.warn( result.duplicateKeys );
+        }
+        // match = Object.fromEntries(destRs.map( (rec) => ([ rec[keyFieldSource], rec ]) ));
+
+        // let notInSourceRS = await destRs.where( this[keyFieldDest].notIn( keyToFind ) ).exec();
+        // notInSource = Object.fromEntries(arrayB.map( (rec) => ([ rec[keyFieldSource], rec ]) ));
+
+        // 
+        let sourceRsChunkNotInDest = sourceRsChunk.filter( (rec) => !match[ rec[keyFieldSource] ] );
+        notInDest = sourceRsChunkNotInDest.length > 0 ? Object.fromEntries( 
+            sourceRsChunkNotInDest.map( (rec) => ([ rec[keyFieldDest], rec ]) ) ) : {};
+
+
+
+        return { 
+            notInSource: notInSource,
+            notInDest: notInDest,
+            match: match,
+            sourceEnd: sourceRsChunk.length < sourcePageSize
+        }
+    }
+
+    async diff( sourceRs, destRs, parameters, chunk = 0 ) {
+        let comparison = this.compare( sourceRs, destRs, parameters, chunk );
+
+        return comparison;
+    }
+
     async  allign( parameters ) {
         
         if ( !parameters.source ) {
