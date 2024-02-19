04f29056cbcb0a5a1440b6ab45bc98a33d8c8635
Alberto Zanelli <albyza85@gmail.com>
2024-02-19 17:22:32 +0100

    compareHelper, TEST: added test for align function (84% Stmts).

--- test/entity-rel/compareHelper.test.js
+++ test/entity-rel/compareHelper.test.js
@@ -26,7 +26,11 @@ describe( "CompareHelper test", function () {
         id: {
             in( keyToFind ) {
                 assert( keyToFind );
-                return true;
+                //return true;
+                return {
+                    in: keyToFind,
+                    f: (e) => ( keyToFind.indexOf(e.id) >= 0 )
+                };
             }
         }
     };
@@ -157,6 +161,177 @@ describe( "CompareHelper test", function () {
             assert( out );
         });
     });
+
+
+    describe( "align test", function () {
+        //
+       it( "align test", async function () {
+           const parameters = {
+            columnMap: (rec) => (rec)
+           };
+
+           const localCustomer = {
+                ...Customer,
+                metaData: {
+                    model: {
+                        fields: {
+                            ...Customer.metaData.model.fields,
+                            id: { equalValues: (a,b) => (a === b) },
+                            //address: { equalValues: (a,b) => (a === b) },
+                        }
+                    }
+                },
+                insert(record) {
+                    assert(record[0].id === 5);
+                    return Promise.resolve(record);
+                },
+                update(id, values) {
+                    assert(id === 2);
+                    assert(values.name === 'UpdateHere');
+                    let result = {
+                        ...values,
+                        id: id
+                    }
+                    return Promise.resolve(result);
+                },
+                delete(records) {
+                    assert(records[0].id === 3);
+                    return Promise.resolve(records);
+                }
+
+           };
+
+           const localSourceQuery = {
+            ...sourceQuery,
+            async exec() {
+                return [
+                    {
+                        id: 1,
+                        name: "nome1",
+                        address: "via nome1",
+                    },
+                    {
+                        id: 2,
+                        name: "UpdateHere",
+                        address: "via Update",
+                    },
+                    {
+                        id: 5,
+                        name: "InsertHere",
+                        address: "via Insert", 
+                    },
+                ];
+            }
+           };
+
+           
+           
+          
+           class FakeQuery {
+           // ...destQuery,
+           constructor () {
+            this.entity = undefined,
+            this.recordSet = [];
+            }
+            page( c, sp ) {
+                assert( c === null || !isNaN( c ) );
+                assert( !isNaN( sp ) );
+            }
+           
+            async exec() {
+                
+                if (!this.whereValue) return this.recordSet;
+
+           /*     let condition = this.whereValue[0];
+
+                 let result = this.recordSet.filter(
+                    condition.f
+                ); */
+
+                let result = this.whereValue.reduce((acc, condition) => {
+
+                    return acc.filter(
+                        condition.f
+                    );
+
+                }, this.recordSet);
+
+
+                return result;
+              
+            }
+            where( condition ) {
+                assert( condition );
+                this.whereValue = [...this.whereValue || [], condition];
+                return this;
+            }
+            andWhere( condition ) {
+                assert( condition );
+                return this.where(condition);
+            }
+            clone() { 
+                let clone = new FakeQuery();
+                clone.entity = this.entity;
+                clone.recordSet = this.recordSet;
+                clone.id = this.id;
+                clone.whereValue = this.whereValue;
+
+                return clone; 
+            }
+         /*    insert(record) {
+                return Promise.resolve(record);
+            } */
+           };
+
+           const localDestQuery = new FakeQuery();
+           localDestQuery.entity = localCustomer;
+           localDestQuery.recordSet =  [
+            {
+                id: 1,
+                name: "nome1",
+                address: "via nome1",
+            },
+            {
+                id: 2,
+                name: "oldValue",
+                address: "via Update",
+            },
+            {
+                id: 3,
+                name: "toDelete",
+                address: "via Delete",
+            },
+        ];
+           localDestQuery.id = {
+               in(keyToFind) {
+                   assert(keyToFind);
+                   return {
+                       in: keyToFind,
+                       f: (e) => (keyToFind.indexOf(e.id) >= 0)
+                   };
+               },
+               notIn(keyToFind) {
+                   assert(Array.isArray(keyToFind));
+                   return {
+                       notIn: keyToFind,
+                       f: (e) => (
+                           keyToFind.indexOf('' + e.id) < 0
+                       )
+                   };
+               }
+           };
+
+           
+        
+
+
+
+           let out = await CompareHelper.align( 
+            localSourceQuery, localDestQuery, parameters );
+
+           assert( out );
+       });
+   });
 
     // copy and uncomment to create a new test
     // describe( "template_descriptor", function () {
