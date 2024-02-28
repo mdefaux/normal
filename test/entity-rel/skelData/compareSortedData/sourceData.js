d929c628e054e998b2da79683392f8c3831c950f
Alberto Zanelli <albyza85@gmail.com>
2024-02-28 17:00:20 +0100

    Added skeleton data in external files for tests.

---
+++ test/entity-rel/skelData/compareSortedData/sourceData.js
@@ -0,0 +1,22 @@
+exports.compareSortedSource = [
+    {
+        id: 1,
+        name: "nome1",
+        address: "via nome1",
+    },
+    {
+        id: 2,
+        name: "UpdateHere",
+        address: "via Update",
+    },
+    {
+        id: 5,
+        name: "InsertHere",
+        address: "via Insert",
+    },
+    {
+        id: 6,
+        name: "sameData",
+        address: "sameHere",
+    },
+];
