4099d29faf42447f19f6c3ae4fcee014705175a0
Alberto Zanelli <albyza85@gmail.com>
2024-04-29 17:08:22 +0200

    index: added CompareHelper and IAlignBuffer to module.exports.

--- SRC/index.js
+++ SRC/index.js
@@ -4,12 +4,16 @@ const {URLquery} = require( './url_query' );
 
 const {KdbStoreHost} = require( './orm/kdbhost/KdbHost' );
 const { Query } = require('./orm/Query');
+const { CompareHelper } = require("./orm/CompareHelper");
+const  { IAlignBuffer }  = require("./orm/IAlignBuffer");
 
 module.exports= {
     URLquery: URLquery,
     store: defs,
     StoreHost: StoreHost,
     Query: Query,
+    CompareHelper: CompareHelper,
+    IAlignBuffer: IAlignBuffer,
     
 
     // KDB
