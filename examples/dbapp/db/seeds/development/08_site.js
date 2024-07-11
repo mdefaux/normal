3f1a148ca849ccff0b76adc03ba80afe4179c101
Maurice Defaux <defauxmaurice@yahoo.com>
2024-07-11 18:15:42 +0200

    dbapp mock data for seeding of device, site and device_model

--- examples/dbapp/db/seeds/development/08_site.js
+++ examples/dbapp/db/seeds/development/08_site.js
@@ -1,3 +1,6 @@
+
+const data = require('../../data/site.json');
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
@@ -5,10 +8,5 @@
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('site').del()
-  await knex('site').insert([
-    { name: "Site 1 for Project 1", project_id: 1 },
-    { name: "Site 1 for Project 1", project_id: 2 },
-    { name: "Site 2 for Project 1", project_id: 1 },
-    { name: "Site 1 for Project 2", project_id: 3 }
-  ]);
+  await knex('site').insert( data );
 };
